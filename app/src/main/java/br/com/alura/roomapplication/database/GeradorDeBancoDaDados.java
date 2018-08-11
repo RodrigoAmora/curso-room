f9b306c1c5bfd49b2b7fefc8c8e04d98de0c113f
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2018-08-11 01:39:19 -0300

    Ajuste

--- app/src/main/java/br/com/alura/roomapplication/database/GeradorDeBancoDaDados.java
+++ app/src/main/java/br/com/alura/roomapplication/database/GeradorDeBancoDaDados.java
@@ -11,7 +11,6 @@ public class GeradorDeBancoDaDados {
                 .allowMainThreadQueries()
                 .fallbackToDestructiveMigration()
                 .build();
-
         return aluraDB;
     }
 
